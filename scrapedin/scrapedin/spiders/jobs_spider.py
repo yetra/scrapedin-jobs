9d8b079dce514cb1aec229240ac39d531e5f1cfc
yetra <brdujmovic@gmail.com>
2022-11-14 19:19:34 +0100

    Follow links to get job descriptions

--- scrapedin/scrapedin/spiders/jobs_spider.py
+++ scrapedin/scrapedin/spiders/jobs_spider.py
@@ -18,10 +18,21 @@ class JobsSpider(scrapy.Spider):
             info = job.css('div.base-search-card__info')[0]
             metadata = info.css('div.base-search-card__metadata')[0]
 
-            yield {
-                'link': remove_url_query_string(extract_with_css(job, 'a::attr(href)')),
+            link = remove_url_query_string(extract_with_css(job, 'a::attr(href)'))
+
+            data = {
+                'link': link,
                 'title': extract_with_css(info, 'h3.base-search-card__title::text'),
                 'subtitle': extract_with_css(info, 'h4.base-search-card__subtitle a::text'),
                 'location': extract_with_css(metadata, 'span.job-search-card__location::text'),
-                'listdate': extract_with_css(metadata, 'time::attr(datetime)')
+                'listdate': extract_with_css(metadata, 'time::attr(datetime)'),
+                'description': response.follow(link, callback=self.parse_description)
             }
+
+            yield scrapy.Request(url=link, callback=self.parse_description, meta=data)
+
+    def parse_description(self, response):
+        description = response.css('div.description__text section div').get()
+        response.meta['description'] = description
+
+        yield response.meta
