ddc10c841276caf33a7ef615308335bd7047d97e
yetra <brdujmovic@gmail.com>
2022-11-14 18:27:27 +0100

    Implement parse method for base job data

--- scrapedin/scrapedin/spiders/jobs_spider.py
+++ scrapedin/scrapedin/spiders/jobs_spider.py
@@ -3,6 +3,19 @@ import scrapy
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
+    start_urls = [
+        'https://www.linkedin.com/jobs/search?keywords=Python&location=Croatia'
+    ]
 
     def parse(self, response, **kwargs):
-        pass
+        for job in response.css('ul.jobs-search__results-list li'):
+            info = job.css('div.base-search-card__info')[0]
+            metadata = info.css('div.base-search-card__metadata')[0]
+
+            yield {
+                'link': job.css('a::attr(href)').get(),
+                'title': info.css('h3.base-search-card__title::text').get(),
+                'subtitle': info.css('h4.base-search-card__subtitle::text').get(),
+                'location': metadata.css('span.job-search-card__location::text').get(),
+                'listdate': metadata.css('time::attr(datetime)').get()
+            }
