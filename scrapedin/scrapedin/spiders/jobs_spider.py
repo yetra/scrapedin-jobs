59cb04815f77cf17c22b7bc3722b37b95d56b17a
yetra <brdujmovic@gmail.com>
2022-11-14 19:02:00 +0100

    Remove scraped URLs' query string

--- scrapedin/scrapedin/spiders/jobs_spider.py
+++ scrapedin/scrapedin/spiders/jobs_spider.py
@@ -11,12 +11,15 @@ class JobsSpider(scrapy.Spider):
         def extract_with_css(selector_obj, css_pattern):
             return selector_obj.css(css_pattern).get(default='').strip()
 
+        def remove_url_query_string(url):
+            return url.split('?')[0]
+
         for job in response.css('ul.jobs-search__results-list li'):
             info = job.css('div.base-search-card__info')[0]
             metadata = info.css('div.base-search-card__metadata')[0]
 
             yield {
-                'link': extract_with_css(job, 'a::attr(href)'),
+                'link': remove_url_query_string(extract_with_css(job, 'a::attr(href)')),
                 'title': extract_with_css(info, 'h3.base-search-card__title::text'),
                 'subtitle': extract_with_css(info, 'h4.base-search-card__subtitle a::text'),
                 'location': extract_with_css(metadata, 'span.job-search-card__location::text'),
