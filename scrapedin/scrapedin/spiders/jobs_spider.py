89874b9e849fa96ad6befc4cd907af1b7898f902
yetra <brdujmovic@gmail.com>
2022-11-14 18:35:39 +0100

    Refactor parse using extract_with_css function

--- scrapedin/scrapedin/spiders/jobs_spider.py
+++ scrapedin/scrapedin/spiders/jobs_spider.py
@@ -8,14 +8,17 @@ class JobsSpider(scrapy.Spider):
     ]
 
     def parse(self, response, **kwargs):
+        def extract_with_css(selector_obj, css_pattern):
+            return selector_obj.css(css_pattern).get(default='').strip()
+
         for job in response.css('ul.jobs-search__results-list li'):
             info = job.css('div.base-search-card__info')[0]
             metadata = info.css('div.base-search-card__metadata')[0]
 
             yield {
-                'link': job.css('a::attr(href)').get(),
-                'title': info.css('h3.base-search-card__title::text').get(),
-                'subtitle': info.css('h4.base-search-card__subtitle::text').get(),
-                'location': metadata.css('span.job-search-card__location::text').get(),
-                'listdate': metadata.css('time::attr(datetime)').get()
+                'link': extract_with_css(job, 'a::attr(href)'),
+                'title': extract_with_css(info, 'h3.base-search-card__title::text'),
+                'subtitle': extract_with_css(info, 'h4.base-search-card__subtitle::text'),
+                'location': extract_with_css(metadata, 'span.job-search-card__location::text'),
+                'listdate': extract_with_css(metadata, 'time::attr(datetime)')
             }
