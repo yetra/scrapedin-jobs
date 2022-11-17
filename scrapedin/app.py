bfcdbfe50079494fabfdfa53c45ae26370d0164b
yetra <brdujmovic@gmail.com>
2022-11-17 18:37:57 +0100

    Run JobsSpider on search

--- scrapedin/app.py
+++ scrapedin/app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+from twisted.internet import reactor
+
+from scrapedin.spiders.jobs_spider import JobsSpider
 
 app = Flask(__name__)
+crawl_runner = CrawlerRunner(get_project_settings())
 
 
 @app.route('/')
@@ -10,4 +16,16 @@ def index():
 
 @app.get('/search')
 def search():
+    d = crawl_runner.crawl(
+        JobsSpider,
+        keywords=request.args.get('keywords', ''),
+        location=request.args.get('location', ''),
+        job_type=','.join(request.args.getlist('job_type')),
+        experience_level=','.join(request.args.getlist('experience_level')),
+        work_type=','.join(request.args.getlist('work_type')),
+    )
+
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run()
+
     return render_template('jobs.html')
