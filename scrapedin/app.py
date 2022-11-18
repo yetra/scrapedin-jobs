91b0f8b2b18f847584d21cd21aac74fa6373c66e
yetra <brdujmovic@gmail.com>
2022-11-18 22:51:21 +0100

    Implement function for handling filter GET requests

--- scrapedin/app.py
+++ scrapedin/app.py
@@ -1,5 +1,6 @@
 import json
 
+import pandas as pd
 from flask import Flask, render_template, request
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
@@ -37,3 +38,21 @@ def search():
 def jobs():
     with open('jobs.json') as json_file:
         return json.load(json_file)
+
+
+@app.get('/filter')
+def filter_jobs():
+    lang_code = request.args.get('lang_code', '')
+    years = request.args.getlist('years_of_experience')
+
+    df = pd.read_json('jobs.json')
+
+    if lang_code:
+        df = df[df.lang_code == lang_code]
+    if years:
+        df = df[df.years_of_experience.isin(years)]
+
+    json_string = df.to_json(orient='table', index=False)
+    json_data = json.loads(json_string)['data']
+
+    return json_data
