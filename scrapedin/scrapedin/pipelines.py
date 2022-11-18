fe15c47131558356e4adf41d426a40248e2f6a9e
yetra <brdujmovic@gmail.com>
2022-11-18 17:30:01 +0100

    Add a pipeline for extracting years of experience

--- scrapedin/scrapedin/pipelines.py
+++ scrapedin/scrapedin/pipelines.py
@@ -5,6 +5,8 @@
 
 
 # useful for handling different item types with a single interface
+import re
+
 import fasttext
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
@@ -31,6 +33,28 @@ class LanguageIdentificationPipeline:
         if description:
             prediction = self.lang_id_model.predict(clean_text(description))
             adapter['lang_code'] = extract_lang_code(prediction)
+        else:
+            raise DropItem(f'Missing description in {item}')
+
+        return item
+
+
+class YearsOfExperiencePipeline:
+    # matches string containing one one-digit number
+    # followed by an optional `+` sign
+    # followed by the strings `year`, `years`, `godina`, or `godine`
+    # (i.e. English and Croatian are the only supported languages)
+    pattern = r'\b(\d)\+? (?:years?|godin[ae])\b'
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        description = adapter.get('description')
+
+        if description:
+            extracted = re.findall(self.pattern, clean_text(description))
+            # the pattern could have multiple matches
+            # so the years of experience are the smallest found number (if any)
+            adapter['years_of_experience'] = min(extracted) if extracted else ''
         else:
             raise DropItem(f'Missing description in {item}')
 
