c5e5b79f4543c37fbf9dd13528a16659eb9fc5a2
yetra <brdujmovic@gmail.com>
2022-11-18 12:35:28 +0100

    Rename listdate to list_date

--- scrapedin/static/jobs.js
+++ scrapedin/static/jobs.js
@@ -22,7 +22,7 @@ function addJob(job, index) {
         <a href="#${tabId}" class="${listItemClass}" id="${listItemId}" data-bs-toggle="list" role="tab" aria-controls="${tabId}">
           <div class="d-flex w-100 justify-content-between">
             <h5 class="mb-1">${job.title}</h5>
-            <small>${job.listdate}</small>
+            <small>${job.list_date}</small>
           </div>
           <p class="mb-1">${job.subtitle}</p>
           <small>${job.location}</small>
@@ -35,7 +35,7 @@ function addJob(job, index) {
                 <div class="card-body">
                   <h5 class="card-title">${job.title}</h5>
                   <p class="card-subtitle mb-2">${job.subtitle}</p>
-                  <small class="text-muted">${job.location} &bull; ${job.listdate}</small>
+                  <small class="text-muted">${job.location} &bull; ${job.list_date}</small>
                   <p class="card-text">
                     ${job.description}
                   </p>
