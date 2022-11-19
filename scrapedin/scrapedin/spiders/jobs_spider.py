db52f407550057afb1a27ac3adc18bd1bbbdf5c4
yetra <brdujmovic@gmail.com>
2022-11-19 13:28:44 +0100

    Move JobsSpider utils back into jobs_spider.py

--- scrapedin/scrapedin/spiders/jobs_spider.py
+++ scrapedin/scrapedin/spiders/jobs_spider.py
@@ -1,7 +1,32 @@
 import scrapy
 
 from scrapedin.items import JobItem
-from scrapedin.spiders.utils import JobSelector, remove_url_query_string, extract_with_css
+
+
+def extract_with_css(selector_obj, css_pattern):
+    return selector_obj.css(css_pattern).get(default='').strip()
+
+
+def remove_url_query_string(url):
+    return url.split('?')[0]
+
+
+class JobSelector:
+    BASE = 'li div.job-search-card'
+
+    INFO = 'div.base-search-card__info'
+    METADATA = 'div.base-search-card__metadata'
+
+    URL = 'a.base-card__full-link::attr(href)'
+    ICON_URL = 'div.search-entity-media img::attr(data-delayed-url)'
+
+    TITLE = 'h3.base-search-card__title::text'
+    SUBTITLE = 'h4.base-search-card__subtitle a::text'
+    LOCATION = 'span.job-search-card__location::text'
+    LIST_DATE = 'time::attr(datetime)'
+    LIST_DATE_TEXT = 'time::text'
+
+    DESCRIPTION = 'div.description__text section div'
 
 
 class JobsSpider(scrapy.Spider):
