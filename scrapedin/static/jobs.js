c1f41d3631b0b81a4f8cd62485417915d72427e3
yetra <brdujmovic@gmail.com>
2022-11-20 12:24:11 +0100

    Update addJob to work with HTML template elements

--- scrapedin/static/jobs.js
+++ scrapedin/static/jobs.js
@@ -31,50 +31,62 @@ function addJob(job, index) {
     let listItemId = `list-item-${index}`;
     let tabId = `tab-${index}`;
 
-    let listItemClass = "list-group-item list-group-item-action";
-    let tabClass = "tab-pane fade";
+    let jobListItem = createJobListItem(index, job, listItemId, tabId);
+    document.querySelector("#job-list").appendChild(jobListItem);
+
+    let jobTab = createJobTab(index, job, listItemId, tabId);
+    document.querySelector("#job-tabs").appendChild(jobTab);
+}
+
+function createJobListItem(index, job, listItemId, tabId) {
+    const template = document.querySelector("#job-list-item");
+    const clone = template.content.cloneNode(true);
+
+    let a = clone.querySelector("a");
+    a.setAttribute("id", listItemId);
+    a.setAttribute("href", `#${tabId}`);
+    a.setAttribute("aria-controls", tabId);
 
     if (index === 0) {
-        listItemClass += " active";
-        tabClass += " show active";
+        a.classList.add("active");
     }
 
-    $("#job-list").append(`
-        <a href="#${tabId}" class="${listItemClass}" id="${listItemId}" data-bs-toggle="list" role="tab" aria-controls="${tabId}">
-          <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${job.title}</h5>
-            <small class="col-auto">${job.list_date_text}</small>
-          </div>
-          <p class="mb-1">${job.subtitle}</p>
-          <small>${job.location}</small>
-        </a>
-    `);
+    clone.querySelector("h5").textContent = job.title;
+    clone.querySelector("p").textContent = job.subtitle;
 
-    $("#job-descriptions").append(`
-        <div class="${tabClass}" id="${tabId}" role="tabpanel" aria-labelledby="${listItemId}">
-          <div class="description card mb-3">
-            <div class="card-body">
-              <div class="row">
-                <div class="col">
-                  <h5 class="card-title">
-                    <a href="${job.url}">
-                      ${job.title}
-                    </a>
-                  </h5>
-                  <p class="card-subtitle mb-2">${job.subtitle}</p>
-                </div>
-                <div class="col-auto text-end">
-                  <img src="${job.icon_url}" alt="">
-                </div>
-              </div>
-              <small class="text-muted">${job.location} &bull; ${job.list_date}</small>
-              <p class="card-text">
-                ${job.description}
-              </p>
-            </div>
-          </div>
-        </div>
-    `);
+    let small = clone.querySelectorAll("small");
+    small[0].textContent = job.list_date_text;
+    small[1].textContent = job.location;
+
+    return clone;
+}
+
+function createJobTab(index, job, listItemId, tabId) {
+    const template = document.querySelector("#job-tab");
+    const clone = template.content.cloneNode(true);
+
+    let firstDiv = clone.querySelector("div");
+    firstDiv.setAttribute("id", tabId);
+    firstDiv.setAttribute("aria-labelledby", listItemId);
+
+    if (index === 0) {
+        firstDiv.classList.add("show");
+        firstDiv.classList.add("active");
+    }
+
+    let h5Link = clone.querySelector("h5 > a");
+    h5Link.setAttribute("href", job.url);
+    h5Link.textContent = job.title;
+
+    let p = clone.querySelectorAll("p");
+    p[0].textContent = job.subtitle;
+    p[1].innerHTML = job.description;
+
+    clone.querySelector("img").setAttribute("src", job.icon_url);
+
+    clone.querySelector("small").innerHTML = `${job.location} &bull; ${job.list_date}`;
+
+    return clone;
 }
 
 function getCheckedSelector(inputName) {
